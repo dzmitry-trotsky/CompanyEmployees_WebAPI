53c1ba0e95f75d074a5771621699395076010a82
dzmitry-trotsky <dzmitry.trotsky@yandex.ru>
2024-07-06 23:12:06 +0300

    FEAT: model validation in post methods

--- Shared/DTO/CompanyForCreationDto.cs
+++ Shared/DTO/CompanyForCreationDto.cs
@@ -1,11 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Shared.DTO
 {
-    public record CompanyForCreationDto(string Name, string Address, string Country,
-        IEnumerable<EmployeeForCreationDto> Employees);
+    public record CompanyForCreationDto
+    {
+        [Required(ErrorMessage = "Company name is required field")]
+        [MaxLength(30, ErrorMessage = "Max length for company name is 30 chars")]
+        public string? Name { get; init; }
+
+        [Required(ErrorMessage = "Company address is required field")]
+        [MaxLength(100, ErrorMessage = "Max length for company address is 100 chars")]
+        public string? Address { get; init; }
+
+        [Required(ErrorMessage = "Company country is required field")]
+        [MaxLength(30, ErrorMessage = "Max length for company country is 30 chars")]
+        public string? Country { get; init; }
+
+        public IEnumerable<EmployeeForCreationDto>? Employees { get; init; }
+
+    }
 }
